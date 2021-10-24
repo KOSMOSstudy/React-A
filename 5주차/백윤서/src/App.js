a5ef5c63db1f27b55299912fe275234109232c48
Baeg <73841260+bgyoons@users.noreply.github.com>
2021-10-24 18:11:09 +0900

    Add files via upload

---
+++ 5주차/백윤서/src/App.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { Link, Route } from "react-router-dom";
+import Home from "./Home";
+import About from "./About";
+import Profile from "./Profile";
+
+function App() {
+  return (
+    <div>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+        <li>
+          <Link to="/profile/bgyoons">bgyoons Profile</Link>
+        </li>
+        <li>
+          <Link to="/profile/ran">ran Profile</Link>
+        </li>
+      </ul>
+      <hr />
+      <Route path="/" component={Home} exact={true} />
+      <Route path={["/about", "/info"]} component={About} />
+      <Route path="/profile/:username" component={Profile} exact={true} />
+    </div>
+  );
+}
+
+export default App;
