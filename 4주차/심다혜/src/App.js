219c2a2692fc9e89dfc7b7853bd2fc9ac8540cf6
shimshim485 <50603345+shimshim485@users.noreply.github.com>
2021-10-24 19:36:57 +0900

    4주차 추가

---
+++ 4주차/심다혜/src/App.js
@@ -0,0 +1,15 @@
+import React,{Component} from 'react';
+import SassComponent from './SassComponent';
+import CSSModule from './CSSModule';
+import StyledComponent from './StyledComponent';
+
+
+class App extends Component{
+  render(){
+    return (
+      <StyledComponent/>
+    );
+  }
+}
+
+export default App;
