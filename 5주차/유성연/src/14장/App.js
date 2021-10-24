d7ef4c80486b9a98dca8d6439de30b08b6a7104b
seongyeon <ysy2565@naver.com>
2021-10-24 21:43:11 +0900

    유성연 md

---
+++ 5주차/유성연/src/14장/App.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import NewsList from './components/NewsList';
+
+const App=()=>{
+    return <NewsList/>;
+}
+export default App;
---
+++ 5주차/유성연/src/14장/components/NewsList.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import styled from 'styled-components';
+import NewsItem from './Newsitem';
+import axios from 'axios';
+import usePromise from '../lib/usePromise';
+
+const NewsListBlock = styled.div`
+  box-sizing: border-box;
+  padding-bottom: 3rem;
+  width: 768px;
+  margin: 0 auto;
+  margin-top: 2rem;
+  @media screen and(max-width: 768px) {
+    width: 100%;
+    padding-left: 1rem;
+    padding-right: 1rem;
+  }
+`;
+const NewsList = () => {
+  return (
+    <NewsListBlock>
+        <NewsItem article={sampleArticle}/>
+        <NewsItem article={sampleArticle}/>
+        <NewsItem article={sampleArticle}/>
+        <NewsItem article={sampleArticle}/>
+        <NewsItem article={sampleArticle}/>
+        <NewsItem article={sampleArticle}/>
+    </NewsListBlock>
+  );
+};
+
+export default NewsList;
